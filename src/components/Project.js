9d21901c962da334e0059c761a2b3f1b23a81aeb
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-14 10:25:34 -0400

    about to add photos

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Project({project}) {
     return (
         <div>
-            <h2></h2>
+            <h2 className="project-name">{project}</h2>
+            <h3>Live Server: 
+                <a target="_blank" href={`https://collerhatguy.github.io/${project}/`}>click here</a>
+            </h3>
+            <h3>Repository: 
+                <a target="_blank" href={`https://github.com/collerhatguy/${project}`}>click here</a>
+            </h3>
+            
         </div>
     )
 }
