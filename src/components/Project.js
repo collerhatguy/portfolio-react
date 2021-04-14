7e600a26ef30c0fdeaf7270549b6739914ebd9d7
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-14 10:33:01 -0400

    added images but they dont render. gonna go work on css

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Project({project}) {
     return (
-        <div>
+        <div className="project">
             <h2 className="project-name">{project}</h2>
             <h3>Live Server: 
                 <a target="_blank" href={`https://collerhatguy.github.io/${project}/`}>click here</a>
@@ -10,7 +10,10 @@ export default function Project({project}) {
             <h3>Repository: 
                 <a target="_blank" href={`https://github.com/collerhatguy/${project}`}>click here</a>
             </h3>
-            
+            <img href={`../projectPhotos/${project}.png`} style={{
+                width: "100px",
+                height: "100px",
+            }}/>
         </div>
     )
 }
