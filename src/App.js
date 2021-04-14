4d7f5f636f4de8fca23f43f258da76826c59741d
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-14 10:18:27 -0400

    everything working so far

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Project from "./components/Project";
 
 function App() {
   const projects = ["hearthstone-react-2", 
-  "coal-miner-fixed", "calculator", "JosephVoelker",
-  "shapeMaker", "christmasTree", "Tyson.css", "emotional-furnace"
+    "coal-miner-fixed", "calculator", 
+    "JosephVoelker", "shapeMaker", 
+    "christmasTree", "Tyson.css", 
+    "emotional-furnace"
 ]
   return (
     <div className="App">
+      {projects.map((project, index) => {
+        return <Project project={project} key={index} />
+      })}
     </div>
   );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Project({project}) {
+    return (
+        <div>
+            <h2></h2>
+        </div>
+    )
+}
