f94d1301150909770317d23aac40da4b2910c06e
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-14 10:13:43 -0400

    project names added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import './App.css';
 
 function App() {
+  const projects = ["hearthstone-react-2", 
+  "coal-miner-fixed", "calculator", "JosephVoelker",
+  "shapeMaker", "christmasTree", "Tyson.css", "emotional-furnace"
+]
   return (
     <div className="App">
     </div>
