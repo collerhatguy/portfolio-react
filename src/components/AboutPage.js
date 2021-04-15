5e125897385bd7b96aa855e3ae3c632ea4ad9f74
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-15 12:07:49 -0400

    things are working well

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AboutPage() {
+    return (
+        <div>
+            I am a self taught web developer that understands and 
+            has done projects with HTML, CSS, JavaScript, and React. 
+            You can expect me to use destructuring, api integration, 
+            async and await, and higher order methods. I am also 
+            attending Lambda’s coding bootcamp. I have been programming 
+            on my own for several months now and plan to do so for my whole life.
+        </div>
+    )
+}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function Project({project, indexOfMain, index}) {
     var className = "";
