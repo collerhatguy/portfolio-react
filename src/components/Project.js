75b4343bb420960e1824fb561b09d73c5c9b1dfe
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-08 09:57:38 -0400

    pisctures finally working

--- src/components/Project.js
+++ src/components/Project.js
@@ -18,9 +18,10 @@ export default function Project({project, indexOfMain, index}) {
                     href={`https://github.com/collerhatguy/${project.name}`}
                 >click here</a>
             </h3>
-            <div 
+           <div 
                 className="project-photo"
-                style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/projectPhotos/${project.name}.png)`}}></div>
+                style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/projectPhotos/${project.name}.png)`}}>
+            </div>
             {/* <img href={`/projectPhotos/${project.name}.png`}></img>
             <blockquote class="imgur-embed-pub" lang="en" data-id="a/jz7cro0" data-context="false" >
                 <a href={project.img}></a>
