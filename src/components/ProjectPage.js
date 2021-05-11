e3852f2f0577abf7b3f7c972c4aeaa6163525e15
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-11 13:58:04 -0400

    new project

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -4,46 +4,16 @@ import useMenuSelector from "../hooks/useMenuSelector";
 
 export default function ProjectPage() {
     const projects = [
-      {
-        name: "hearthstone-react-2",
-        img: "https://imgur.com/jz7cro0",
-      },
-      {
-        name: "coal-miner-fixed",
-        img: "https://imgur.com/3GrWKTx",
-      },
-      {
-        name: "face-maker-2",
-        img: "https://imgur.com/3GrWKTx",
-      },
-      {
-        name: "zelda",
-        img: "https://imgur.com/3GrWKTx",
-      },
-      {
-        name:  "calculator",
-        img: "https://imgur.com/eFniWMh",
-      },
-      {
-        name: "JosephVoelker",
-        img: "https://imgur.com/zfoAHBA",
-      },
-      {
-        name: "shapeMaker",
-        img: "https://imgur.com/n52OH0B",
-      },
-      {
-        name: "christmasTree",
-        img: "https://imgur.com/yKDi1wU",
-      },
-      {
-        name: "Tyson.css",
-        img: "https://imgur.com/wnVFxSw",
-      },
-      {
-        name: "emotional-furnace",
-        img: "https://imgur.com/9Y1wUOW",
-      }
+                    "hearthstone-react-2",
+                    "coal-miner-fixed",
+                    "face-maker-2",
+                    "zelda",
+                    "calculator",
+                    "front-end-mentor-1",
+                    "JosephVoelker",
+                    "shapeMaker",
+                    "christmasTree",
+                    "Tyson.css",
     ]
     const [handleProjectLeft, handleProjectRight, mainProject] = useMenuSelector(projects);
     return (
